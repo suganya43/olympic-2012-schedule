ec94a9c3fb2784792f456834bbb10a25e995ca92
unknown <suganya.s@.(none)>
2012-07-24 17:59:18 +0530

    seed push

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
   belongs_to :event
-  attr_accessible :date, :name, :participants, :sport_id, :status, :time, :venue, :winner
+  attr_accessible :date, :event_name, :round_name,:participants, :sport_id, :status, :time, :venue, :winner
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+country = Country.create([{name:'India'},{name:'Afghanistan'},{name:'London'}])
+sport = Sport.create([{name:'Archery'},{name:'Athletic'},{name:'Badminton'},{name:'Hockey'}])
+evnt = Event.create([{event_name:'Mens individual',sport_id:1,round_name:'first',date:'2001-09-28',time:'03:00:00',venue:'London'},{event_name:'Wo-Mens individual',sport_id:2,round_name:'first',date:'2001-09-28',time:'03:00:00',venue:'London'},{event_name:'Mens Team',sport_id:3,round_name:'first',date:'2001-09-28',time:'03:00:00',venue:'London'},{event_name:'Wo-Mens Team',sport_id:4,round_name:'first',date:'2001-09-31',time:'03:00:00',venue:'London'}])
